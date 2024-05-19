e91d5d1f38e013e65f1d8c4cb9004a99f876ce13
Carolina Rutili de Lima <carolina.rutililima@gmail.com>
2024-05-19 15:42:24 -0300

    new

---
+++ MY_MAZE/Q learning + TFG/app_q_TFG.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pyamaze import maze,agent,COLOR
+from env_q_TFG_goback_begging_v1 import MyAlgorithm
+#from env_q_TFG_old import MyAlgorithm
+
+
+# List of colors of the agents
+colorList = [COLOR.red, COLOR.blue, COLOR.yellow, COLOR.cyan, COLOR.black]
+
+numOfLines = 5
+numOfColumns = 5
+n_agents = 1
+m=maze(numOfLines, numOfColumns)
+m.CreateMaze(loadMaze= 'maze_5x5.csv' ,theme=COLOR.light)
+maze_map = m.maze_map
+
+
+
+
+
+myAlgorithm = MyAlgorithm(m, n_agents,  maze_map, colorList, (numOfLines, numOfColumns))
+steps, pionner_steps, fraction, fraction_pionner = myAlgorithm.run() 
